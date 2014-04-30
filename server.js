48032552687cb74532d8a0721791b95fd6121820
Jonnie Spratley <jonniespratley@gmail.com>
2014-04-29 20:28:05 -0700

    updated db

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 var RestServer = require('./lib/rest-server.js').RestServer;
-
+var DbServer = require('./lib/db-server.js').DbServer;
 
 var port = 9191;
-var server = new RestServer();
-	server.listen(port);
+var _server = new RestServer();
+	_server.listen(port);
+
+
+var _db = new DbServer({
+    models:{
+        'post': {
+            title: String,
+            slug: String,
+            body: String,
+            image: String,
+            published: Boolean,
+            tags: Array,
+            created: Date,
+            modified: Date
+        }
+    }
+});
+
+    _db.connect('localhost/learning-yeoman');
 
 console.log('Server running @ localhost:'+port);
+
+_db.findAll('posts')
+_db.findOne('posts', 1)
