586f4fc1d8853e769f3232ee46524a11d39f577e
Jonnie Spratley <jonniespratley@me.com>
2014-04-30 20:10:42 -0700

    updated dbserver

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ var port = 9191;
 var _server = new RestServer();
 _server.listen( port );
 
+/***
+ * DB Usage
+ * @type {DbServer}
+ * @private
+ */
 var _db = new DbServer( {
 	models: {
 		'posts': {
@@ -16,20 +21,30 @@ var _db = new DbServer( {
 			tags: Array,
 			created: Date,
 			modified: Date
-		}
+		},
+		'pages': { title: String, slug: String, body: String, published: Boolean},
+		'users': { username: String, password: String, email: String, active: Boolean}
 	}
 } );
 
 _db.connect( 'localhost/learning-yeoman' );
-_db.findAll( 'posts' );
-_db.findOne( 'posts', 1 );
+
+var posts = _db.findAll( 'posts' );
+console.log(posts);
+
+var newId = _db.create( 'posts', {
+	title: 'New Post',
+	body: 'This is the body of the post.',
+	created: new Date()
+} );
 
 
-var newId = _db.create( 'posts', {title: 'New Post', body: 'This is the body of the post.', created: new Date()} );
+if (newId) {
+	console.log( 'Post created', newId );
+	_db.destroy( 'posts', newId._id );
 
-if(newId){
-	console.log('Post created', newId);
-	_db.destroy( 'posts', newId.id);
+	_db.findOne( 'posts', 1 );
+
 }
 
 console.log( 'Server running @ localhost:' + port );
